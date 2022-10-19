969a13509f66752500aa278b46ce05996257439c
varrietasotelo <valen65a@hotmail.com>
2022-10-19 09:33:21 -0500

    upload the script
    no ha sido posible subir el env directamente puesto que contiene mas de 100 archivos

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+stores = [
+  {
+    "name": "Valentina_Store",
+    "items": [
+      {
+        "name": "Table",
+        "price": 10.99
+      }
+    ]
+  } 
+]  
+
+@app.get("/store") # esta es el endpoint http://127.0.0.1:5000/store
+def get_stores():
+    return {"stores": stores}
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201 #the code 201 means that the date has been store
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404 #the code 404 means not found
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+           
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
